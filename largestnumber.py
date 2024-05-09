67f06cc1b89854fc4a1f72fb09b54d84ab19cdf8
Tanvi Rainak <76578879+tanvirnk136@users.noreply.github.com>
2024-05-09 11:35:42 +0530

    Update largestnumber.py

--- largestnumber.py
+++ largestnumber.py
@@ -2,6 +2,6 @@ a=10
 b=30
 
 if(a>b):
-	print("The largest number is a" ,a)
+	print("The largest number is a:" ,a)
 else:
-	print("The largest number is b", b)
+	print("The largest number is b:", b)
